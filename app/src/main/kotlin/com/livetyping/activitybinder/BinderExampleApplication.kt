1d3ee1fec6699480b2cfa82410cc27ea8adb8c7b
Dmitry Alexeeenkoff <d.alekseenkov@ltst.ru>
2018-09-14 18:49:06 +0600

    example: added dep to needed modules

---
+++ app/src/main/kotlin/com/livetyping/activitybinder/BinderExampleApplication.kt
@@ -0,0 +1,20 @@
+package com.livetyping.activitybinder
+
+import android.app.Application
+import com.livetyping.logincore.SocialLoginBinder
+import com.livetyping.vk.VkNetwork
+
+class BinderExampleApplication : Application() {
+
+    val socialLoginBinder: SocialLoginBinder by lazy {
+        SocialLoginBinder()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        socialLoginBinder.initializeSocialNetworks(this,
+                listOf(
+                        VkNetwork()
+                ))
+    }
+}
--- app/src/main/kotlin/com/livetyping/activitybinder/SocialActivity.kt
+++ app/src/main/kotlin/com/livetyping/activitybinder/SocialActivity.kt
@@ -2,12 +2,26 @@ package com.livetyping.activitybinder
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.livetyping.logincore.SocialLoginBinder
+import com.livetyping.vk.VkLoginRoute
+import kotlinx.android.synthetic.main.activity_social.*
 
 
 class SocialActivity : AppCompatActivity() {
 
+    private lateinit var socialLoginBinder: SocialLoginBinder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social)
+
+        socialLoginBinder = (application as BinderExampleApplication).socialLoginBinder // can be injected with dagger
+
+        login_vk.setOnClickListener {
+            socialLoginBinder.loginWith(VkLoginRoute()) {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
