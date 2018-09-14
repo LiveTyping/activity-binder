b3dd29726e9a73f7191884eeb2c0b638000b791d
Dmitry Alexeeenkoff <d.alekseenkov@ltst.ru>
2018-09-14 13:48:04 +0600

    global: work on gradle settings of modules

---
+++ social/vk/src/main/kotlin/com/livetyping/vk/SocialLoginError.kt
@@ -0,0 +1,6 @@
+package com.livetyping.utils.social
+
+
+interface SocialLoginError {
+    fun error(): String
+}
---
+++ social/vk/src/main/kotlin/com/livetyping/vk/SocialNetwork.kt
@@ -0,0 +1,9 @@
+package com.livetyping.utils.social
+
+import android.app.Application
+
+
+interface SocialNetwork {
+
+    fun init(app: Application)
+}
