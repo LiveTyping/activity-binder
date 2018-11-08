288125862a0f5c996859170a8a61c464377f175a
Kirill Starostin <k.starostin@ltst.ru>
2018-11-08 10:10:33 +0600

    Remove facebook login method's initialization
    The initialization methods are now called internally by the sdk itself on app's start

--- social/facebook/src/main/kotlin/com/livetyping/facebook/FacebookInitializer.kt
+++ social/facebook/src/main/kotlin/com/livetyping/facebook/FacebookInitializer.kt
@@ -1,14 +1,13 @@
 package com.livetyping.facebook
 
 import android.app.Application
-import com.facebook.appevents.AppEventsLogger
 import com.livetyping.logincore.SocialInitializer
 
 
 class FacebookInitializer : SocialInitializer {
 
     override fun init(app: Application) {
-        AppEventsLogger.activateApp(app)
+        // pass
     }
 
 }
