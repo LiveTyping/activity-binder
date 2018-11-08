76475bbcfed6fc1ec4eb8765ee396fa7fd294585
Kirill Starostin <k.starostin@ltst.ru>
2018-11-08 12:54:55 +0600

    Add request to get an access token, pass it to success block

--- social/google/src/main/java/com/livetyping/google/GoogleNetwork.kt
+++ social/google/src/main/java/com/livetyping/google/GoogleNetwork.kt
@@ -1,7 +1,9 @@
 package com.livetyping.google
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import com.google.android.gms.auth.GoogleAuthUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
@@ -13,11 +15,14 @@ class GoogleNetwork(serverClientId: String) : SocialNetwork {
         private const val GOOGLE_SIGN_IN_ACTIVITY_REQUEST_CODE = 231
     }
 
+    private var context: Context? = null
+
     private val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestServerAuthCode(serverClientId)
             .build()
 
     override fun login(activity: Activity) {
+        context = activity
         val signInClient = GoogleSignIn.getClient(activity, signInOptions)
         activity.startActivityForResult(signInClient.signInIntent, GOOGLE_SIGN_IN_ACTIVITY_REQUEST_CODE)
     }
@@ -29,9 +34,12 @@ class GoogleNetwork(serverClientId: String) : SocialNetwork {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                successBlock(account!!.serverAuthCode!!)
+                val accessToken = GoogleAuthUtil.getToken(context, account!!.account, null)
+                successBlock(accessToken)
             } catch (e: ApiException) {
                 errorBlock?.invoke(GoogleLoginError(e))
+            } finally {
+                context = null
             }
         }
     }
