6ba183dad0517b700f109ac65fc06fad774ebeca
Dmitry Alexeeenkoff <d.alekseenkov@ltst.ru>
2019-01-24 14:57:24 +0600

    Add multiple photo choice from gallery

--- app/src/main/kotlin/com/livetyping/activitybinder/ImagesExampleActivity.kt
+++ app/src/main/kotlin/com/livetyping/activitybinder/ImagesExampleActivity.kt
@@ -3,6 +3,7 @@ package com.livetyping.activitybinder
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.livetyping.images.ImagesBinder
 import kotlinx.android.synthetic.main.activity_images.*
@@ -26,6 +27,11 @@ class ImagesExampleActivity : AppCompatActivity() {
             imagesBinder.takeThumbnailFromCamera("rationale text for active permission", "settings")
             { bitmap ->
                 image.setImageBitmap(bitmap)
+            }
+        }
+        multiple_gallery.setOnClickListener {
+            imagesBinder.multiplePickFromGallery {
+                Toast.makeText(this, it.size.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
