af2952a9ed139ecb96695381aca292e01ea49e05
Dmitry Alexeeenkoff <d.alekseenkov@ltst.ru>
2019-01-24 13:24:08 +0600

    Add images activity button

--- app/src/main/kotlin/com/livetyping/activitybinder/MainActivity.kt
+++ app/src/main/kotlin/com/livetyping/activitybinder/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_images.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
