a388fc8d1d981995dd611f4d9544c9db887c90a8
dsatyanov <d.satyanov@ltst.ru>
2019-08-23 10:52:39 +0600

    Refactor social binder; Delete instagram; Update sdks' versions; Add versions.gradle file

--- social/google/src/main/java/com/livetyping/google/GoogleLoginError.kt
+++
@@ -1,9 +0,0 @@
-package com.livetyping.google
-
-import com.google.android.gms.common.api.ApiException
-import com.livetyping.logincore.SocialLoginError
-
-class GoogleLoginError(private val apiException: ApiException) : SocialLoginError {
-
-    override fun error() = apiException.message.orEmpty()
-}
--- social/vk/src/main/kotlin/com/livetyping/vk/VkNetwork.kt
+++ social/vk/src/main/kotlin/com/livetyping/vk/VkNetwork.kt
@@ -2,34 +2,52 @@ package com.livetyping.vk
 
 import android.app.Activity
 import android.content.Intent
-import com.livetyping.logincore.SocialLoginError
 import com.livetyping.logincore.SocialNetwork
-import com.vk.sdk.VKAccessToken
-import com.vk.sdk.VKCallback
-import com.vk.sdk.VKScope
-import com.vk.sdk.VKSdk
-import com.vk.sdk.api.VKError
-
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.*
+import com.vk.api.sdk.auth.VKAuthCallback.Companion.AUTH_CANCELED
+import com.vk.api.sdk.auth.VKScope.*
 
 class VkNetwork : SocialNetwork<VkLoginResult> {
 
-    override fun login(activity: Activity) {
-        VKSdk.login(activity, VKScope.EMAIL)
+    companion object {
+        private const val VK_AUTH_CANCELLED = "VK SDK: authorization canceled"
+        private const val VK_UNKNOWN_ERROR = "VK SDK: Unknown error"
     }
 
-    override fun onActivityResult(requestCode: Int,
-                                  resultCode: Int, data: Intent?,
-                                  successBlock: (result: VkLoginResult) -> Unit,
-                                  errorBlock: ((error: SocialLoginError) -> Unit)?) {
-        if (!VKSdk.onActivityResult(requestCode, resultCode, data, object : VKCallback<VKAccessToken> {
-                    override fun onResult(vkAccessToken: VKAccessToken) {
-                        successBlock.invoke(vkAccessToken.toVkLoginResult())
-                    }
+    override fun login(activity: Activity) {
+        VK.login(activity, listOf(EMAIL))
+    }
 
-                    override fun onError(error: VKError) {
-                        errorBlock?.invoke(VkLoginError(error))
-                    }
-                })) {
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        onSuccess: (result: VkLoginResult) -> Unit,
+        onFail: (exception: Exception) -> Unit
+    ) {
+        VK.onActivityResult(
+            requestCode,
+            resultCode,
+            data,
+            vkAuthCallback(onSuccess, onFail)
+        )
+    }
+
+    private fun vkAuthCallback(
+        onSuccess: (result: VkLoginResult) -> Unit,
+        onFail: (exception: Exception) -> Unit
+    ) = object : VKAuthCallback {
+
+        override fun onLogin(token: VKAccessToken) {
+            val result = token.toVkLoginResult()
+            onSuccess.invoke(result)
+        }
+
+        override fun onLoginFailed(errorCode: Int) {
+            val errorMessage = if (errorCode == AUTH_CANCELED) VK_AUTH_CANCELLED else VK_UNKNOWN_ERROR
+            val exception = Exception(errorMessage)
+            onFail.invoke(exception)
         }
     }
 }
