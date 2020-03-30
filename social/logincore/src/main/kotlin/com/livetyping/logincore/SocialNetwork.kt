8990a5b8789bafb79d57cc201c5ca7e64697f179
alexeenkoff <alexeenkoff@gmail.com>
2020-03-30 23:44:49 +0300

    Merge LiveTyping-develop

--- social/logincore/src/main/kotlin/com/livetyping/logincore/SocialNetwork.kt
+++ social/logincore/src/main/kotlin/com/livetyping/logincore/SocialNetwork.kt
@@ -3,12 +3,15 @@ package com.livetyping.logincore
 import android.app.Activity
 import android.content.Intent
 
-
 interface SocialNetwork<T> {
 
     fun login(activity: Activity)
 
-    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?,
-                         successBlock: (result: T) -> Unit,
-                         errorBlock: ((error: SocialLoginError) -> Unit)? = null)
+    fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?,
+        onSuccess: (result: T) -> Unit,
+        onFail: (exception: Exception) -> Unit
+    )
 }
